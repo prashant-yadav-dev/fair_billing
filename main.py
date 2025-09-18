7e37084de04d7b85b167f6f90a39c0d37c065851
Prashant Yadav <prashant.yadav@demandscience.com>
2025-09-18 14:27:37 +0530

    Added fibo series code.

---
+++ main.py
@@ -0,0 +1,11 @@
+# write a code for fibonacci series
+def fibonacci(n):
+    a, b = 0, 1
+    series = []
+    for _ in range(n):
+        series.append(a)
+        a, b = b, a + b
+    return series
+
+
+print(fibonacci(10))
